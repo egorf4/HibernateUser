063dfc01c4468aaa50c9bd13cc5dccaf373e34d3
egorf4 <e.fedorovich4@gmail.com>
2024-11-07 14:47:57 +0300

    PreProject 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+private final static String DB_URL = "jdbc:mysql://localhost:3306/mydb_schema1";
+private final static String DB_USER = "user";
+private final static String DB_PASSWORD = "user";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Can't connect to database or load driver!");
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
